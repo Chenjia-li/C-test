3f3082d2a24d6c57de88bea8e4288821828c60b4
Chenjia-li <2385428982@qq.com>
2022-04-15 17:20:59 +0800

    动态分配内存

---
+++ test003.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int number;
+    int* a;
+    int i;
+    // printf("请输入数量: ");
+    scanf("%d",&number);
+    // int a[number];
+    
+    // a =(int*)malloc(number*sizeof(int));
+    
+    // for(i = 0;i<number;i++){
+    //     scanf("%d",&a[i]);
+    // }
+    // for(i = number-1;i>=0;i--){
+    //     printf("%d ",a[i]);
+    // }
+
+    // free(a);//释放空间
+
+    return 0 ;
+}
